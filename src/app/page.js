62021778a441b5e81a1cf277247f7f13e90efc66
Syed Farhad Reza <syedfarhad71@gmail.com>
2026-06-22 11:14:12 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,6 @@ import { TopInstructors } from "@/Component/TopInstructors";
 export default function Home() {
   return (
     <>
-    <h1>HI</h1>
-
     <Banner></Banner>
     <PopularCourses></PopularCourses>
     <LearningTips></LearningTips>
